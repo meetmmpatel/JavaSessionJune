59aad15847eb838a0d3e8d8bb87a45ef7828d1e0
Milan Patel <milanpatel@milan.local>
2018-06-13 11:38:12 -0400

    Update in SampletTest class file

--- src/com/javasession/week/two/tryandcatch/SampleTest.java
+++ src/com/javasession/week/two/tryandcatch/SampleTest.java
@@ -2,23 +2,6 @@ package com.javasession.week.two.tryandcatch;
 
 public class SampleTest {
 
-	public static void main(String[] args) {
-		System.out.println("checkt the bonus durarion");
-		checkEligibity(13,99);
-		
-		
-	}
 	
-	static void checkEligibity(int empId, int empNum) {
-		if (empId < 12 && empNum < 40) {
-			throw new ArithmeticException("Not eligible for bonus");
-			
-		}
-		else {
-			System.out.println("Eligible for bonus");
-		}
-		
-		
-	}
 
 }
